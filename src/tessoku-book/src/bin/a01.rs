39322e658b382b1490bd47bc61dcd7f42ce99e4c
sayoi341 <me@sayoi341.moe>
2023-10-28 18:18:23 +0900

    add tessokbook

---
+++ src/tessoku-book/src/bin/a01.rs
@@ -0,0 +1,20 @@
+#![allow(unused_imports)]
+use itertools::{Itertools, MinMaxResult};
+use proconio::{input, marker::*};
+use std::{
+    cmp::{max, min},
+    collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque},
+    io::*,
+    iter::{FromIterator, IntoIterator},
+    ops::{Add, Div, Mul, Neg, Sub},
+    str::FromStr,
+    vec,
+};
+
+fn main() {
+    input! {
+        n: usize,
+    }
+    
+    println!{"{}", n * n};
+}
