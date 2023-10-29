8622164dceccafd05ac96398628075237b88a868
sayoi341 <me@sayoi341.moe>
2023-10-29 19:12:15 +0900

    format

--- .devcontainer/cargo.sh
+++ .devcontainer/cargo.sh
@@ -4,10 +4,10 @@ set -xe
 
 /usr/local/cargo/bin/cargo install cargo-compete
 /usr/local/cargo/bin/cargo install cargo-member
-/usr/local/cargo/bin/cargo compete i atcoder 2
+/usr/local/cargo/bin/cargo compete i atcoder
 
 /usr/local/cargo/bin/rustup component add rustfmt
 /usr/local/cargo/bin/cargo install cargo-snippet --features="binaries"
 
 echo login to atcoder
-# cargo compete l atcoder
+/usr/local/cargo/bin/cargo compete l atcoder
