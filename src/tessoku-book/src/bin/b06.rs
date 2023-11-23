55c53526b558e6df731a58cb2f8cb02a52a4deec
sayoi341 <me@sayoi341.moe>
2023-11-24 00:08:58 +0900

    solve b06

--- src/tessoku-book/src/bin/b06.rs
+++ src/tessoku-book/src/bin/b06.rs
@@ -13,6 +13,33 @@ use std::{
 
 fn main() {
     input! {
+        n: usize,
+        aa: [usize; n],
+        q: usize,
+        lr: [(usize, usize); q],
     }
-    println!("Hello World");
+
+    let mut acc = vec![0; n + 1];
+
+    for i in 0..n {
+        acc[i + 1] = acc[i];
+        if aa[i] == 0 {
+            acc[i + 1] -= 1;
+        } else {
+            acc[i + 1] += 1;
+        }
+    }
+
+    for (l, r) in lr {
+        let ans = acc[r] - acc[l - 1];
+
+        println!(
+            "{}",
+            match ans {
+                0 => "draw",
+                _ if ans > 0 => "win",
+                _ => "lose",
+            }
+        );
+    }
 }
