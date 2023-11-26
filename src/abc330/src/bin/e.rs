ce7f5d1767734820808c5e7d0fe2c5cea26264dd
sayoi341 <me@sayoi341.moe>
2023-11-27 01:52:22 +0900

    solve abc330 e

--- src/abc330/src/bin/e.rs
+++ src/abc330/src/bin/e.rs
@@ -1,4 +1,5 @@
 #![allow(unused_imports)]
+use im_rc::{hashmap, hashset};
 use itertools::{Itertools, MinMaxResult};
 use proconio::{fastout, input, marker::*};
 use std::{
@@ -16,26 +17,41 @@ fn main() {
     input! {
         n: usize,
         q: usize,
-        aa: [usize; n],
+        mut aa: [usize; n],
         ix: [(Usize1, usize); q],
     }
 
-    let mut map: BTreeMap<usize, usize> = BTreeMap::new();
-    for (i, &a) in aa.iter().enumerate() {
-        *map.entry(i).or_insert(0) = a;
+    // Aに含まれない数字の集合
+    let mut set: BTreeSet<usize> = BTreeSet::from_iter(0..=n);
+    // Aに含まれる数字の出現回数(key = 数字, value = 出現回数)
+    let mut map: HashMap<usize, usize> = HashMap::new();
+
+    for &a in aa.iter() {
+        *map.entry(a).or_insert(0) += 1;
+        set.remove(&a);
     }
 
-    for (i, x) in ix {
-        *map.entry(i).or_insert(0) = x;
-        let max = map.iter().max_by_key(|x| x.1).unwrap().1 + 1;
+    for &(i, x) in ix.iter() {
+        // A[i]をxに変更するクエリを処理する
 
-        's: for i in 0..=max {
-            let mut vec = map.values().clone().collect_vec();
-            vec.sort();
-            if vec.binary_search(&&i).is_err() {
-                println!("{}", i);
-                break 's;
-            }
+        // A[i]の出現回数を減らす
+        *map.entry(aa[i]).or_insert(0) -= 1;
+
+        // A[i]がAに含まれなくなったら、Aに含まれない数字の集合に追加する
+        if *map.get(&aa[i]).unwrap() == 0 {
+            set.insert(aa[i]);
         }
+
+        // xの出現回数を増やす
+        *map.entry(x).or_insert(0) += 1;
+
+        // A[i]をxに変更する
+        aa[i] = x;
+
+        // xをAに含まれない数字の集合から削除する
+        set.remove(&x);
+
+        // 最小値を出力する
+        println!("{}", set.iter().next().unwrap());
     }
 }
