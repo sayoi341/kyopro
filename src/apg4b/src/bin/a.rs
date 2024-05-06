f769ab3a3b5c3fbbbaf90799cde6d091df8bfce0
sayoi341 <me@sayoi341.moe>
2024-05-06 14:54:16 +0000

    solve apg4b

---
+++ src/apg4b/src/bin/a.rs
@@ -0,0 +1,16 @@
+#![allow(unused_imports)]
+use ac_library::*;
+use itertools::{Itertools, MinMaxResult};
+use proconio::{marker::*, *};
+use std::{
+  cmp::{max, min},
+  collections::*,
+  io::*,
+  iter::{FromIterator, IntoIterator},
+  ops::{Add, Div, Mul, Neg, Sub},
+  str::FromStr,
+};
+
+fn main() {
+  println!("Hello, world!");
+}
