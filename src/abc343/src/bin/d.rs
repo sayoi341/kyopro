6542ead6f8ab4cf64653e7035ba9360dd0392211
sayoi341 <me@sayoi341.moe>
2024-07-02 01:31:21 +0000

    solve: ✅ abc343-d

--- src/abc343/src/bin/d.rs
+++ src/abc343/src/bin/d.rs
@@ -14,6 +14,23 @@ use superslice::Ext;
 
 fn main() {
   input! {
+    n: usize,
+    t: usize,
+    abarr: [(Usize1, usize); t],
   }
-  println!("Hello World");
+
+  let mut map = BTreeMap::new();
+  map.entry(0).or_insert(n);
+  let mut scores = vec![0; n];
+
+  for (player, add_score) in abarr {
+    let current_score = scores[player];
+    map.entry(current_score).and_modify(|v| *v -= 1);
+    if map[&current_score] == 0 {
+      map.remove(&current_score);
+    }
+    scores[player] += add_score;
+    map.entry(scores[player]).and_modify(|v| *v += 1).or_insert(1);
+    println!("{}", map.len());
+  }
 }
